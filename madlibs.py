737ecf53a497c82255047b1517abfc2104574bd4
Jasmine Hernandez <bezierbee@gmail.com>
2023-10-02 18:04:22 -0400

    completed madlibs.py modified program

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+noun=input("Whats your favorite food?")
+print("My favorite food is " + str(noun) )
+
+adjective=input("Describe in one word how ice cream taste?")
+print("Ice cream taste " + str(adjective) + " to me")
+
+adverb=input("What kind of day comes after 'today'?")
+print(str(adverb) + " comes after today")
+
+verb=input("Which speed do you walk?")
+print("I walk very " + str(verb) )
+	
+
+
+
